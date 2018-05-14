9b6455cf68882f23d4eace7b5b96ea764648c89e
Hardik Lakdawala <hardik@socialpilot.co>
2018-05-14 16:10:23 +0530

    Added eslint and related dev dependencies

---
+++ react-src/src/index.jsx
@@ -0,0 +1,13 @@
+/* global document */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import store from './store';
+import './index.css';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
--- react-src/src/reducer/profile.js
+++ react-src/src/reducer/profile.js
@@ -1,20 +1,18 @@
-export default function profile(state={}, action) {
-    let newState;
-    switch(action.type) {
+export default function profile(state = {}, action) {
+  let newState;
+  switch (action.type) {
+    case 'FETCH_PROFILES_SUCCESS':
+      newState = { ...state, data: action.payload };
+      break;
 
-        case 'FETCH_PROFILES_SUCCESS' :
-            newState = {...state, data: action.payload};       
-        break;
+    case 'FETCH_PROFILES_ERROR':
+      newState = { ...state, data: action.payload };
+      break;
 
-        case 'FETCH_PROFILES_ERROR' :
-            newState = {...state, data: action.payload};
-        break;
+    default:
+      newState = { ...state };
+      break;
+  }
 
-        default:
-            newState = {...state};
-        break;
-    
-    }
-
-    return newState;
-};
+  return newState;
+}
