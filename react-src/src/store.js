ca636a8a50e31653c2a141df17c8df0cd0288790
Hardik Lakdawala <hardik@socialpilot.co>
2018-05-15 09:47:00 +0530

    refactored store file with eslint rule

--- react-src/src/store.js
+++ react-src/src/store.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import posts from './reducer/posts';
 import profile from './reducer/profile';
 
-import thunk from 'redux-thunk';
+
 const middleware = applyMiddleware(thunk);
 const reducers = combineReducers({
-    posts,
-    profile
+  posts,
+  profile,
 });
 
 export default createStore(reducers, {}, middleware);
